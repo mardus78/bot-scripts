e048dde2c118fb5037d8b71faf093718eedd4d4f
mardus78 <145954738+mardus78@users.noreply.github.com>
2023-09-25 23:53:26 -0400

    Update botScript.js

--- botScript.js
+++ botScript.js
@@ -1,9 +1,3 @@
-document.getElementById('userInput').addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) {  // checks if the pressed key is "Enter"
-        sendMessage();
-    }
-});
-
 function sendMessage() {
     let input = document.getElementById('userInput').value;
     let chatHistory = document.getElementById('chatHistory');
@@ -11,28 +5,33 @@ function sendMessage() {
     // Display user message
     chatHistory.innerHTML += '<div>User: ' + input + '</div>';
 
-    // Simple bot response logic
-    if (input.toLowerCase().includes('name')) {
-        chatHistory.innerHTML += '<div>Bot: What is your name?</div>';
-    } else if (input.toLowerCase().includes('email')) {
-        chatHistory.innerHTML += '<div>Bot: What is your email address?</div>';
-    } else {
-        chatHistory.innerHTML += '<div>Bot: I didn\'t understand that. Please provide your name or email.</div>';
+    // Bot logic for Cleaning Contractors
+    if (input.toLowerCase().includes('how can i join')) {
+        chatHistory.innerHTML += '<div>Bot: To join our platform, you\'ll need to sign up, complete your profile, and upload relevant business documents like insurance and WSIB. Once verified, you\'ll be listed and potential clients can view your services and reviews.</div>';
+    } else if (input.toLowerCase().includes('fee to join')) {
+        chatHistory.innerHTML += '<div>Bot: We offer both free and premium listings. The premium listing comes with added benefits like featured placement. Would you like more details on the pricing?</div>';
+    } else if (input.toLowerCase().includes('get reviews')) {
+        chatHistory.innerHTML += '<div>Bot: Customers who avail your services through our platform can leave reviews. Encourage your clients to provide feedback to build your reputation.</div>';
     }
 
-    // Send data to the server
-    fetch('http://localhost:3000/send-email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ message: input })
-    });
+    // Bot logic for Visitors
+    else if (input.toLowerCase().includes('find a cleaner')) {
+        chatHistory.innerHTML += '<div>Bot: You can browse through our list of verified cleaning contractors, view their profiles, services offered, and reviews to make an informed choice.</div>';
+    } else if (input.toLowerCase().includes('cleaners verified')) {
+        chatHistory.innerHTML += '<div>Bot: Yes, all our cleaning contractors are required to upload their business documents, which we verify. You can also view reviews from other customers for added assurance.</div>';
+    } else if (input.toLowerCase().includes('type of cleaning')) {
+        chatHistory.innerHTML += '<div>Bot: We have cleaners specializing in residential, commercial, and deep cleaning. What type of cleaning service are you looking for?</div>';
+    }
+
+    // ... (You can continue to expand the logic based on more scenarios)
+
+    // Fallback response
+    else {
+        chatHistory.innerHTML += '<div>Bot: I\'m sorry, I didn\'t understand that. Can you rephrase or provide more details?</div>';
+    }
 
     // Clear the input field
     document.getElementById('userInput').value = '';
 }
 
-// This line should be at the very end:
-document.getElementById('sendBotMessage').addEventListener('click', sendMessage);
 
