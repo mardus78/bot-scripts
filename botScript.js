cf2acb6b8b3547f8c52a4c9af4fe2025c42c091d
mardus78 <145954738+mardus78@users.noreply.github.com>
2023-09-29 23:16:35 -0400

    Update botScript.js

--- botScript.js
+++ botScript.js
@@ -5,41 +5,37 @@ function sendMessage() {
     // Display user message
     chatHistory.innerHTML += '<div>User: ' + input + '</div>';
 
-    // Bot logic for Cleaning Contractors
-    if (input.toLowerCase().includes('how can i join')) {
-        chatHistory.innerHTML += '<div>Bot: To join our platform, you\'ll need to sign up, complete your profile, and upload relevant business documents like insurance and WSIB. Once verified, you\'ll be listed and potential clients can view your services and reviews.</div>';
-    } else if (input.toLowerCase().includes('fee to join')) {
-        chatHistory.innerHTML += '<div>Bot: We offer both free and premium listings. The premium listing comes with added benefits like featured placement. Would you like more details on the pricing?</div>';
-    } else if (input.toLowerCase().includes('get reviews')) {
-        chatHistory.innerHTML += '<div>Bot: Customers who avail your services through our platform can leave reviews. Encourage your clients to provide feedback to build your reputation.</div>';
-    }
-
-    // Bot logic for Visitors
-    else if (input.toLowerCase().includes('find a cleaner')) {
-        chatHistory.innerHTML += '<div>Bot: You can browse through our list of verified cleaning contractors, view their profiles, services offered, and reviews to make an informed choice.</div>';
-    } else if (input.toLowerCase().includes('cleaners verified')) {
-        chatHistory.innerHTML += '<div>Bot: Yes, all our cleaning contractors are required to upload their business documents, which we verify. You can also view reviews from other customers for added assurance.</div>';
-    } else if (input.toLowerCase().includes('type of cleaning')) {
-        chatHistory.innerHTML += '<div>Bot: We have cleaners specializing in residential, commercial, and deep cleaning. What type of cleaning service are you looking for?</div>';
-    }
-
-    // Fallback response
-    else {
-        chatHistory.innerHTML += '<div>Bot: I\'m sorry, I didn\'t understand that. Can you rephrase or provide more details?</div>';
-    }
+    // Call Wit.ai API
+    fetch('https://api.wit.ai/message?v=20230927&q=' + encodeURIComponent(input), {
+        headers: {
+            'Authorization': 'Bearer RKEK5XAE5PDHHJZHYU57LTIQM2ANSG4S'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Process the Wit.ai response and determine the reply
+        let reply = processWitResponse(data);
+        chatHistory.innerHTML += '<div>Bot: ' + reply + '</div>';
+    })
+    .catch(error => {
+        console.error('Error calling Wit.ai:', error);
+        chatHistory.innerHTML += '<div>Bot: Sorry, I had trouble processing that. Please try again.</div>';
+    });
 
     // Clear the input field
     document.getElementById('userInput').value = '';
 }
 
-// Event listeners
-document.getElementById('sendBotMessage').addEventListener('click', sendMessage);
-
-document.getElementById('userInput').addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) {  // checks if the pressed key is "Enter"
-        sendMessage();
+function processWitResponse(data) {
+    // Here, you'll extract information from the Wit.ai response and determine the bot's reply.
+    // For now, let's just return the first intent's name as a placeholder.
+    if (data.intents && data.intents.length > 0) {
+        return data.intents[0].name;
+    } else {
+        return "I'm not sure how to respond to that.";
     }
-});
+}
+
 
 
 
